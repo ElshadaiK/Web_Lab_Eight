6a1ded14f493316dae9790f52f7a06f5dea3e905
Elshadai <elshadaikassutegegn@gmail.com>
2021-02-17 22:55:00 +0300

    Template literals finished

--- asset/js/01(TemplateLiteral).js
+++ asset/js/01(TemplateLiteral).js
@@ -15,13 +15,13 @@ function templateStringConcatenation() {
     const student = { name: "Abebe", age: 21 }
 
     //String to Display 
-    const fullIntro = "My Name is " + student.name + " and i am " + student.age + " Years Old"
+    const fullIntro = "My Name is " + student.name + " and I am " + student.age + " Years Old"
 
     //1. Create a variable called fullIntroTemplate and Store the same string displayed above with template literal
-
+    let fullIntroTemplate = `My Name is ${student.name} and I am ${student.age} Years Old`
 
     //3. Display it [change it to fullIntroTemplate]
-    templateConcatenationResult.innerHTML = fullIntro;
+    templateConcatenationResult.innerHTML = fullIntroTemplate;
 
 }
 
@@ -32,10 +32,10 @@ function displayTemplateExpression() {
     const result = "The Sum of the Numbers is  : " + sum(1, 2)
 
     //1. Create a variable called result1 and Store the same string displayed above with template literal
-
+    let result1 = `The Sum of the Numbers is  : ${sum(1, 2)}`
 
     //3. Display it [change it to result1]
-    templateExpression.innerHTML = result;
+    templateExpression.innerHTML = result1;
 
 
 
