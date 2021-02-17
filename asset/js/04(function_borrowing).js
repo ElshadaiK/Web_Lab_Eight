f7301287b32f5029f84e7cf08f57ac70e8f7fbed
Elshadai <elshadaikassutegegn@gmail.com>
2021-02-18 00:01:10 +0300

    Functions finished

--- asset/js/04(function_borrowing).js
+++ asset/js/04(function_borrowing).js
@@ -29,7 +29,8 @@ function usingCall() {
     }
 
     //1. Borrow fullName using call
-    //2. Pass call_demo[as ui_place] , Hi I am ,[message]
+    Brad.fullName.call(Cerscy, call_demo, 'Hi I am ')
+        //2. Pass call_demo[as ui_place] , Hi I am ,[message]
 
 
 }
@@ -45,6 +46,8 @@ function usingApply() {
     //1. Borrow fullName using apply
     //2. Pass apply_demo[as ui_place] and Hi I am ,[as message] as array
 
+    Brad.fullName.apply(Jon, [apply_demo, 'Hi I am '])
+
 
 }
 
@@ -58,6 +61,8 @@ function usingBind() {
 
     //1. Borrow fullName using bind
     //2. Pass bind_demo[as ui_place] , Hi I am ,[as message]
+    let newFun = Brad.fullName.bind(Daenerys, bind_demo, 'Hi I am ')
+    newFun()
 
 
 }
